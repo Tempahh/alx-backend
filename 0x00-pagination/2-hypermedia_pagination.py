0a5cd1fe816d701bae68554ba087d7ce3edfc3d0
Prosper <prosperorjiogo1999@gmail.com>
2024-06-29 10:57:39 +0100

    pagination coreection

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -84,8 +84,8 @@ class Server:
             else len(page_data),
             "page": page,
             "data": page_data,
-            "next_page": page + 1 if page >= 0 else None,
-            "prev_page": page - 1 if page >= 1 else None,
+            "next_page": page + 1 if page >= 0 and page <= len(page_data) else None,
+            "prev_page": page - 1 if page >= 1 and page -1 != 0 else None,
             "total_pages": total_pages
         }
         return hyper_dict
