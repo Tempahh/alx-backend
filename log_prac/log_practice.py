f804470e016a53bb018c9584a121859f3913be4a
Prosper <prosperorjiogo1999@gmail.com>
2024-08-01 00:18:52 +0100

    queues

--- log_prac/log_practice.py
+++ log_prac/log_practice.py
@@ -1,4 +1,6 @@
 import logging
+import re
+from typing import List
 
 #debug: Detailed information, typically of interest only when diagnosing problems.
 
@@ -9,6 +11,8 @@ import logging
 #error: Due to a more serious problem, the software has not been able to perform some function.
 
 #critical: A serious error, indicating that the program itself may be unable to continue running.
+
+logger = logging.getLogger(__name__)
 
 logging.basicConfig(filename='test.log',level=logging.DEBUG, format='%(levelname)s:%(asctime)s:%(message)s')
 
@@ -22,7 +26,7 @@ def subtract(a, b):
     return a - b
 
 add_res = add(a=89, b=456789)
-logging.debug(f"Add: 89 + 456789 = {add_res}")
+logger.debug(f"Add: 89 + 456789 = {add_res}")
 
 
 class employee:
@@ -33,8 +37,45 @@ class employee:
         if self.first == '' or self.last == '':
             logging.warning('Employee name is not provided')
             return
-        logging.debug(f"Created Employee: {self.first} {self.last}")
+        logger.debug(f"Created Employee: {self.first} {self.last}")
         
 
 emp1 = employee('John', 'Doe')
 emp2 = employee('', 'Smith')
+
+
+def filter_datum(fields, redaction, message, separator):
+    #this function returns the log message obfuscating the fields provided in the fields list
+    #with the redaction string using re.sub method to perform the substitution with a single regex
+    
+    for field in fields:
+        message = re.sub(field+'=.*?'+separator, 
+                         field+'.*?'+redaction+separator, message)
+        
+    return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        redact the message of LogRecord instance
+        Args:
+        record (logging.LogRecord): LogRecord instance containing message
+        Return:
+            formatted string
+        """
+        message = super(RedactingFormatter, self).format(record)
+        redacted = filter_datum(self.fields, self.REDACTION,
+                                message, self.SEPARATOR)
+        return redacted
